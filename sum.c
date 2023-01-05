559a6aabf4972275b2d9842691d85d4ad86ad23c
Vishal Bidikar <v7vishalbidikar@gmail.com>
2023-01-05 16:18:40 +0530

    substraction operation

--- sum.c
+++ sum.c
@@ -2,11 +2,14 @@
 
 void main()
 {
-    int a,b,sum;
+    int a,b,sum1,sum2;
     printf("Enter the 1st number to be added");
     scanf("%d",&a);
     printf("Enter the 2nd number to be added");
     scanf("%d",&b);
-    sum = a+b;
-    printf("sum is %d",sum);
+    sum1 = a+b;
+    // substraction
+    sum2 = a-b;
+    printf("sum is %d",sum1);
+    printf("sum is %d",sum2);
 }
